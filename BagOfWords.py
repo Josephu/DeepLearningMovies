e682d3d7c7953005e9ce0f14f88aa99a40b24500
Jimmy Wong <jimmy.wong@fairfaxmedia.com.au>
2015-06-15 23:30:54 +1000

    Add cross validation features and try different regression models

--- BagOfWords.py
+++ BagOfWords.py
@@ -10,21 +10,24 @@
 # *************************************** #
 
 import os, pdb, csv
+from datetime import datetime
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn import cross_validation
 from KaggleWord2VecUtility import KaggleWord2VecUtility
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 import numpy as np
 
 def main():
+    start_time = datetime.now()
+
     df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', 'labeledTrainData.tsv'), header=0, delimiter="\t", quoting=3)
     # test = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', 'testData.tsv'), header=0, delimiter="\t", \
     #                quoting=3 )
-    msk = np.random.rand(len(df)) < 0.6
-    train = df[msk].reset_index(drop=True)
-    test = df[~msk].reset_index(drop=True)
-
+    train, test, train_sentiment, test_sentiment = cross_validation.train_test_split(df['review'].values, df['sentiment'].values, test_size=0.4, random_state=0)
 
     print 'Download text data sets. If you already have NLTK datasets downloaded, just close the Python download window...'
     #nltk.download()  # Download text data sets, including stop words
@@ -36,8 +39,8 @@ def main():
     # of the movie review list
 
     print "Cleaning and parsing the training set movie reviews...\n"
-    for i in xrange(0, len(train["review"])):
-        clean_train_reviews.append(" ".join(KaggleWord2VecUtility.review_to_wordlist(train["review"][i], True)))
+    for i in xrange(0, len(train)):
+        clean_train_reviews.append(" ".join(KaggleWord2VecUtility.review_to_wordlist(train[i], True)))
 
 
     # ****** Create a bag of words from the training set
@@ -56,7 +59,7 @@ def main():
     # Tfidf: a normalization method to reduce weight of words that appear too frequent in dataset
     # TfidfVectorizer: CountVectorizer that run a tfidf normalization during transform
     vectorizer = TfidfVectorizer(min_df=3, max_features=None, strip_accents='unicode', analyzer='word', token_pattern=r'\w{1,}',
-                                 ngram_range=(1, 1), use_idf=1, smooth_idf=1, sublinear_tf=1, stop_words='english')
+                                 ngram_range=(1, 2), use_idf=1, smooth_idf=1, sublinear_tf=1, stop_words='english')
 
     # fit_transform() does two functions: First, it fits the model
     # and learns the vocabulary; second, it transforms our training data
@@ -71,49 +74,60 @@ def main():
     # array
     train_data_features = train_data_features.toarray()
 
-    # ******* Train a random forest using the bag of words
+    # ******* Train a model using the bag of words
     #
-    print "Training the random forest (this may take a while)..."
+    print "Training the model (this may take a while)..."
 
 
     # Initialize a Random Forest classifier with 100 trees
-    forest = RandomForestClassifier(n_estimators=100)
+    # clf = RandomForestClassifier(n_estimators=100)
+    # clf = svm.LinearSVC(C=1)
+    clf = LogisticRegressionCV(cv=3, scoring='roc_auc', solver='liblinear')
 
-    # Fit the forest to the training set, using the bag of words as
+    # Cross validation, this takes a long time ...
+    # print "4 Fold CV Score: ", np.mean(cross_validation.cross_val_score(clf, train_data_features, train_sentiment, cv=4, scoring='accuracy', n_jobs=4))
+
+    # Fit the svc to the training set, using the bag of words as
     # features and the sentiment labels as the response variable
     #
     # This may take a few minutes to run
-    forest = forest.fit(train_data_features, train["sentiment"])
-
-
+    model = clf.fit(train_data_features, train_sentiment)
 
     # Create an empty list and append the clean reviews one by one
     clean_test_reviews = []
 
     print "Cleaning and parsing the test set movie reviews...\n"
-    for i in xrange(0, len(test["review"])):
-        clean_test_reviews.append(" ".join(KaggleWord2VecUtility.review_to_wordlist(test["review"][i], True)))
+    for i in xrange(0, len(test)):
+        clean_test_reviews.append(" ".join(KaggleWord2VecUtility.review_to_wordlist(test[i], True)))
 
     # Get a bag of words for the test set, and convert to a numpy array
     test_data_features = vectorizer.transform(clean_test_reviews)
     test_data_features = test_data_features.toarray()
 
-    # Use the random forest to make sentiment label predictions
+    # Use svc to make sentiment label predictions
     print "Predicting test labels...\n"
-    result = forest.predict(test_data_features)
+    result = model.predict(test_data_features)
 
     # Copy the results to a pandas dataframe with an "id" column and
     # a "sentiment" column
-    output = pd.DataFrame(data={"id":test["id"], "sentiment":test["sentiment"], "predict_sentiment":result})
+    output = pd.DataFrame(data={"sentiment":test_sentiment, "predict_sentiment":result})
     output['succeed'] = output['sentiment'] == output['predict_sentiment']
 
     groupby = output.groupby('succeed')
     print 'Result Evaluation'
-    print groupby['id'].agg(['count'])
+    print groupby['sentiment'].agg(['count'])
 
     # Use pandas to write the comma-separated output file
     output.to_csv(os.path.join(os.path.dirname(__file__), 'data', 'Bag_of_Words_model.csv'), index=False, quoting=csv.QUOTE_MINIMAL)
     print "Wrote results to Bag_of_Words_model.csv"
+
+    print datetime.now() - start_time
+    print 'Cs_'
+    print getattr(model, 'Cs_')
+    print 'scores_'
+    print getattr(model, 'scores_')
+    print 'C_'
+    print getattr(model,'C_')
 
 if __name__ == "__main__":
     main()
--- linear.py
+++ linear.py
@@ -1,4 +1,5 @@
-import os
+import os, pdb
+from datetime import datetime
 from KaggleWord2VecUtility import KaggleWord2VecUtility
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
@@ -7,22 +8,22 @@ import pandas as pd
 import numpy as np
 
 def main():
+    start_time = datetime.now()
+
     df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', 'labeledTrainData.tsv'), header=0, delimiter="\t", quoting=3)
     # test = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', 'testData.tsv'), header=0, delimiter="\t", quoting=3)
-    msk = np.random.rand(len(df)) < 0.6
-    train = df[msk].reset_index(drop=True)
-    test = df[~msk].reset_index(drop=True)
-    y = train['sentiment']
+    train, test, y, y_test = cross_validation.train_test_split(df['review'].values, df['sentiment'].values, test_size=0.4, random_state=0)
+
     print "Cleaning and parsing movie reviews...\n"      
     traindata = []
-    for i in xrange(0, len(train['review'])):
-        traindata.append(" ".join(KaggleWord2VecUtility.review_to_wordlist(train["review"][i], False)))
+    for i in xrange(0, len(train)):
+        traindata.append(" ".join(KaggleWord2VecUtility.review_to_wordlist(train[i], False)))
     testdata = []
-    for i in xrange(0, len(test['review'])):
-        testdata.append(" ".join(KaggleWord2VecUtility.review_to_wordlist(test["review"][i], False)))
+    for i in xrange(0, len(test)):
+        testdata.append(" ".join(KaggleWord2VecUtility.review_to_wordlist(test[i], False)))
     print 'vectorizing... ',
     tfv = TfidfVectorizer(min_df=3, max_features=None, strip_accents='unicode', analyzer='word', token_pattern=r'\w{1,}',
-                          ngram_range=(1, 1), use_idf=1, smooth_idf=1, sublinear_tf=1, stop_words='english')
+                          ngram_range=(1, 2), use_idf=1, smooth_idf=1, sublinear_tf=1, stop_words='english')
     X_all = traindata + testdata
     lentrain = len(traindata)
 
@@ -36,7 +37,7 @@ def main():
     model = LogisticRegression(penalty='l2', dual=True, tol=0.0001,
                                C=1, fit_intercept=True, intercept_scaling=1.0,
                                class_weight=None, random_state=None)
-    print "20 Fold CV Score: ", np.mean(cross_validation.cross_val_score(model, X, y, cv=20, scoring='roc_auc'))
+    print "10 Fold CV Score: ", np.mean(cross_validation.cross_val_score(model, X, y, cv=10, scoring='roc_auc'))
 
     print "Retrain on all training data, predicting test labels...\n"
     model.fit(X, y)
@@ -45,16 +46,18 @@ def main():
     # output = pd.DataFrame( data={"id":test["id"], "sentiment":result} )
 
     # Copy the results to a pandas dataframe with an "id" column and a "sentiment" column
-    output = pd.DataFrame(data={"id":test["id"], "sentiment":test["sentiment"], "predict_sentiment":result})
+    output = pd.DataFrame(data={"sentiment":y_test, "predict_sentiment":result})
     output['succeed'] = output['sentiment'] == output['predict_sentiment']
 
     groupby = output.groupby('succeed')
     print 'Result Evaluation'
-    print groupby['id'].agg(['count'])
+    print groupby['sentiment'].agg(['count'])
 
     # Use pandas to write the comma-separated output file
     output.to_csv(os.path.join(os.path.dirname(__file__), 'data', 'Bag_of_Words_model_linear.csv'), index=False, quoting=3)
     print "Wrote results to Bag_of_Words_model_linear.csv"
+
+    print datetime.now() - start_time
 
 if __name__ == "__main__":
     main()
